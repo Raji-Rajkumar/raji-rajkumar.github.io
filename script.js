582cfaaeae667710df9ac0da368ac51849b2491e
Raji-Rajkumar <36248432+Raji-Rajkumar@users.noreply.github.com>
2019-08-09 11:54:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
     $('#dismiss, .overlay').on('click', function () {
         $('#sidebar').removeClass('active');
         $('.overlay').removeClass('active');
+        $('#navbar-brand').show();
     });
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').addClass('active');
         $('.overlay').addClass('active');
+        $('#navbar-brand').hide();
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
